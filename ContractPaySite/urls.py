1010c440021964134cb2226c8d3b8cc8319f272a
Bobby Nicholson <bobbynicholson78704@gmail.com>
2017-11-12 14:54:26 -0600

    Add files via upload

--- ContractPaySite/urls.py
+++ ContractPaySite/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
+
+
+
 urlpatterns = [
+
     url(r'^admin/', admin.site.urls),
     url(r'^contractor/', include('contractor.urls'))
 ]
